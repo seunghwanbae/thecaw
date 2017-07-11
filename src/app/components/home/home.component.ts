29090cbf31f150f79775b0bce38a8e2f6550a623
seunghwan <seunghwan@Seunghwan-MacBook-Pro.local>
2017-07-11 17:43:30 +0900

    Animation test done

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,43 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
 	selector: 'app-home',
 	templateUrl: './home.component.html',
-	styleUrls: ['./home.component.scss'],
-	animations: [
-    trigger('animationState', [
-      state('inactive', style({
-      	// overflow: 'hidden',
-      	display: 'none',
-      	height: '0',
-        // transform: 'translateX(0) scale(1)'
-      })),
-      state('active',   style({
-      	display: 'block',
-      	height: '*'/*,
-      	overflow: '*'*/
-        // transform: 'translateX(50%) scale(2)'
-      })),
-      transition('* <=> *', animate('500ms ease-in-out'))
-    ])
-  ]
+	styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-	animationStateNow: string = 'inactive';
 	constructor() { }
 
 	ngOnInit() {
-	}
-
-	animationStart( event ) {
-		console.log('toState:' + event.toState, 'start');
-	}
-	animationDone( event ) {
-		console.log('toState:' + event.toState, 'end');
-	}
-	toggleState(){
-		this.animationStateNow = (this.animationStateNow === 'inactive') ? 'active' : 'inactive';
 	}
 
 }
