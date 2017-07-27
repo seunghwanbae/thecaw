e6144d64fe7ac6ce324639d3996bb38647c1cdfa
seunghwan <seunghwan@Seunghwan-MacBook-Pro.local>
2017-07-27 13:33:05 +0900

    Update ui css

--- src/app/components/global-header/global-header.component.ts
+++ src/app/components/global-header/global-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-global-header',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./global-header.component.scss']
 })
 export class GlobalHeaderComponent implements OnInit {
-
-  constructor() { }
+  @ViewChild('test') testCompo;
+  constructor(private el: ElementRef) { }
 
   ngOnInit() {
+
+    console.log(this.el.nativeElement)
   }
 
 }
